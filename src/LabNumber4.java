d6d5b5fb35711d0b1343427c64b3397f760b0e33
jillxx <jiaoxu84@gmail.com>
2018-04-25 17:34:47 -0400

    adding comments

--- src/LabNumber4.java
+++ src/LabNumber4.java
@@ -1,4 +1,4 @@
-//Jill Xu (pair programm with
+//Jill Xu (pair program with Ben McBrayer)
 //This program display a table of powers.
 import java.util.Scanner;
 
@@ -7,16 +7,21 @@ public class LabNumber4 {
 		Scanner scnr = new Scanner(System.in);
 		int num;
 		String cont = "y";
-
+        
+		
 		System.out.println("Learn your squares and cubes!");
+		
 		while (cont.equalsIgnoreCase("y")) {
+			//prompt user to enter an integer
 			System.out.print("Enter an integer: ");
 			num = scnr.nextInt();
+			//display the header in a format.
 			System.out.printf("%1$-9s %2$-10s %3$-10s", "Number", "Squared", "Cubed");
 
-
+            //display the dividing line
 			System.out.printf("\n%1$-9s %2$-10s %3$-10s", "=======", "=======", "======");
 			
+			//calculate the squared and cubed number and casting it into integer
 			for (int i = 1; i <= num; i++) {
 
 				int Squared = (int) Math.pow(i, 2);
@@ -24,6 +29,7 @@ public class LabNumber4 {
 				System.out.printf("\n%1$-9d %2$-10d %3$-10d", i, Squared, Cubed);
 				
 			}
+			//ask the user if he wants too continue
 			System.out.println("\nContinue?(y/n): ");
 			cont = scnr.next();
 
